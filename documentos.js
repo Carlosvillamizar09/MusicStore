0d1a0b4cf7e287b88a252048a6881d2fbceee9cd
Carlosvillamizar09 <carlosvila0903@gmail.com>
2025-08-11 08:09:39 -0500

    feat: :sparkles: Se añade documentos.js
    Donde se insertan los documentos que tendra esta coleccion

---
+++ documentos.js
@@ -0,0 +1,188 @@
+db.discos.insertMany([{
+    "sku":"M101",
+    "title": "Back in Black",
+    "artist": "AC/DC",
+    "genre": "Rock",
+    "release_year": "1980",
+    "format": "Vinilo",
+    "price": 95000,
+    "stock": 8,
+    "rating": 4.9,
+    "available": true,
+    "origin": "importado",
+    "tags": ["clasico", "coleccionable", "edicion especial"],
+    "label": { 
+        "name": "Atlantic Records",
+        "country": "Estados Unidos"
+    }
+},{
+    "sku":"M034",
+    "title": "Judas",
+    "artist": "Lady gaga",
+    "genre": "Pop",
+    "release_year": "2010",
+    "format": "CD",
+    "price": 50000,
+    "stock": 5,
+    "rating": 5.0,
+    "available": true,
+    "origin": "importado",
+    "tags": ["joya", "coleccionable", "edicion limitada"],
+    "label": { 
+        "name": "Jujutsu Kaisen",
+        "country": "Estados Unidos"
+    }
+},{
+    "sku":"M018",
+    "title": "Blue Bird",
+    "artist": "Naruto",
+    "genre": "Anime",
+    "release_year": "2004",
+    "format": "CD",
+    "price": 20000,
+    "stock": 30,
+    "rating": 4.3,
+    "available": true,
+    "origin": "importado",
+    "tags": ["innovador", "coleccionable", "edicion plateada"],
+    "label": { 
+        "name": "Konoha",
+        "country": "Japon"
+    }
+},{
+    "sku":"M059",
+    "title": "Te Bote",
+    "artist": "Bad Bunny",
+    "genre": "Trap",
+    "release_year": "2018",
+    "format": "CD",
+    "price": 55000,
+    "stock": 12,
+    "rating": 3.9,
+    "available": true,
+    "origin": "importado",
+    "tags": ["nuevo",  "edicion conejo"],
+    "label": { 
+        "name": "Tiny",
+        "country": "Puerto Rico"
+    }
+}, {
+    "sku":"M012",
+    "title": "Pretextos",
+    "artist": "Maluma",
+    "genre": "Reggaeton",
+    "release_year": "2014",
+    "format": "CD",
+    "price": 60000,
+    "stock": 4,
+    "rating": 4.7,
+    "available": true,
+    "origin": "importado",
+    "tags": ["nuevo", "maluma baby", "edicion pretty boy"],
+    "label": { 
+        "name": "Atlantic Music",
+        "country": "Colombia"
+    }
+},{
+    "sku":"M025",
+    "title": "El rey",
+    "artist": "Vicente Fernandez",
+    "genre": "Ranchera",
+    "release_year": "1982",
+    "format": "Vinilo",
+    "price": 105000,
+    "stock": 2,
+    "rating": 4.4,
+    "available": true,
+    "origin": "importado",
+    "tags": ["clasico", "coleccionable", "edicion oro"],
+    "label": { 
+        "name": "Pedrito",
+        "country": "Mexico"
+    }
+},{
+    "sku":"M007",
+    "title": "Bring me to life",
+    "artist": "Goku",
+    "genre": "Rock",
+    "release_year": "2000",
+    "format": "CD",
+    "price": 77777,
+    "stock": 7,
+    "rating": 7,
+    "available": true,
+    "origin": "importado",
+    "tags": ["esencia", "7 palabras", "edicion esencial"],
+    "label": { 
+        "name": "Dragon ball",
+        "country": "Japon"
+    }
+},{
+    "sku":"M043",
+    "title": "Wait in White",
+    "artist": "Marvel/Studios",
+    "genre": "Rock",
+    "release_year": "1980",
+    "format": "Vinilo",
+    "price": 95000,
+    "stock": 8,
+    "rating": 2,
+    "available": true,
+    "origin": "importado",
+    "tags": ["clasico", "coleccionable", "edicion especial"],
+    "label": { 
+        "name": "Disney",
+        "country": "Estados Unidos"
+    }
+},{
+    "sku":"M102",
+    "title": "Tonight the music",
+    "artist": "Deadpool",
+    "genre": "Jazz",
+    "release_year": "1999",
+    "format": "Vinilo",
+    "price": 90000,
+    "stock": 0,
+    "rating": 5,
+    "available": true,
+    "origin": "importado",
+    "tags": ["clasico", "romantico", "edicion especial"],
+    "label": { 
+        "name": "Waza",
+        "country": "Inglaterra"
+    }
+},{
+    "sku":"M001",
+    "title": "G.O.A.T",
+    "artist": "Crisiano Ronaldo",
+    "genre": "Rap",
+    "release_year": "1985",
+    "format": "Vinilo",
+    "price": 500000,
+    "stock": 1,
+    "rating": 5,
+    "available": true,
+    "origin": "importado",
+    "tags": ["clasico", "coleccionable", "edicion goat"],
+    "label": { 
+        "name": "CR7",
+        "country":"Portugal"
+    }
+},{    
+    "sku":"M111",
+    "title": "Thriller",
+    "artist": "Michael Jackson",
+    "genre": "Rock",
+    "release_year": "1980",
+    "format": "Vinilo",
+    "price": 195000,
+    "stock": 10,
+    "rating": 4.9,
+    "available": true,
+    "origin": "importado",
+    "tags": ["clasico", "coleccionable", "edicion especial"],
+    "label": { 
+        "name": "Ayuwoki",
+        "country": "Estados Unidos"
+    }
+}])
